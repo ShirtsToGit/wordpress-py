32fe395ff1e0fd8ad78f130a29c6e3d05048170f
Eddie Webb <ollitech@gmail.com>
2017-08-02 13:54:18 -0400

    walks catalog and uses meta.json to detect if new or existing

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+#! /usr/bin/env python
 import os
+import simplejson as json
 from wp.api import Wordpress
 import wpconfig
 
-wp = Wordpress("https://test.shirtstogit.com",wpconfig.key,wpconfig.secret)
+api = Wordpress("https://test.shirtstogit.com",wpconfig.key,wpconfig.secret)
+catalog = "shirts"
+cwd = os.getcwd()
 
-posts = wp.posts()
-print posts
+# r = wp.products()
+# print "All Product\n" + r.text
+# r = wp.products("resist")
+# print "Search Product\n" + r.text
+for root, dirs, filenames in os.walk(catalog):
+	for entry in filenames:
+		filename = root + "/" + entry
+		print "Inspecting " + filename
+		if(entry == "meta.json"):
+			file = open(filename,'r')
+			data = file.read()
+			meta = json.loads(data);
+			if(meta['slug']):
+				api.publish_design(meta)
+			else:
+				print "No slug defined in meta.json"
+
+	
--- wp/api.py
+++ wp/api.py
@@ -17,5 +17,27 @@ class Wordpress(object):
 		    version="wp/v2"
 		)
 
-	def posts(self):
-		return self.wpapi.get("posts")
+	def publish_design(self,meta):
+		slug = meta['slug']
+		print "Publishing design: " + slug
+		r = self.product_by_slug("its-gneiss")
+		if(r.status_code == 200):
+			#product exists, update
+			print "UPdating: " + slug
+		else:
+			#publish new product.
+			print "Publishing new product at " + slug
+
+
+
+
+
+	def products(self,search=""):
+		path = "products"
+		if(search):
+			path =  "products?search=" + search
+		return self.wpapi.get(path)
+
+	def product_by_slug(self,slug):
+		path =  "products?slug=" + slug
+		return self.wpapi.get(path)
