62e45675b9b3082b85ea4853d9a99408823bcbed
Eddie Webb <ollitech@gmail.com>
2017-08-14 18:02:12 -0400

    store slug optional in validator

--- validator.py
+++ validator.py
@@ -3,7 +3,9 @@ import requests
 
 def validate(meta,path,store_prefix):
 	check_slug_validity(meta,path)
-	store_url=store_prefix + meta['store_slug']
+	store_url=store_prefix + meta['slug']
+	if "store_slug" in meta:
+		store_url=store_prefix + meta['store_slug']
 	check_store_exists(meta,store_url)
 
 
